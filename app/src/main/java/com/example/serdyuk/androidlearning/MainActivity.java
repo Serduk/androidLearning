025e43835679bed2458e12e7eb65ea977b679c48
Serdyuk <serduksergey90@gmail.com>
2017-11-12 22:00:58 +0200

    fix and update beerchoose app

--- app/src/main/java/com/example/serdyuk/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/serdyuk/androidlearning/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.serdyuk.androidlearning;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickButton(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner brands_arr = (Spinner) findViewById(R.id.brands_arr);
+
+        brands.setText(brands_arr.getSelectedItem().toString());
+
+        System.out.println("Clicked!");
+        System.out.println(brands_arr.getSelectedItem());
     }
 }
